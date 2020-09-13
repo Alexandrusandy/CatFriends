c8939df4641ecbb48d244da49edcce9f8f012057
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-09-13 12:29:12 +0300

    Updates

---
+++ precache-manifest.8e1628fe6a82b44d54263287747bbbe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e430e70ab79a5a5eef7cf2338c1042cc",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "7e7818df6a7c52bf6da4",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8047f00fec93e04984c5",
+    "url": "/RoboFriends/static/css/main.8f46f518.chunk.css"
+  },
+  {
+    "revision": "7e7818df6a7c52bf6da4",
+    "url": "/RoboFriends/static/js/2.4b3f96cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.4b3f96cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8047f00fec93e04984c5",
+    "url": "/RoboFriends/static/js/main.7869f25a.chunk.js"
+  },
+  {
+    "revision": "676b6babe186f06ed4c3",
+    "url": "/RoboFriends/static/js/runtime-main.a032a3a4.js"
+  }
+]);
