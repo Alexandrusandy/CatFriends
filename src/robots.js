86138d3d99378b6e83d54f8761a7a80108e0d1a4
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-31 21:31:20 +0200

    catfriends

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,363 @@
 export const robots = [
   {
     id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
+    name: 'Alex',
+    
+    email: 'alex@gmail.com'
   },
   {
     id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv'
+    name: 'Sandy',
+   
+    email: 'sandel@gmail.com'
   },
   {
     id: 3,
-    name: 'Clementine Bauch',
-    username: 'Samantha',
-    email: 'Nathan@yesenia.net'
+    name: 'Wolf ',
+    
+    email: 'wolfdog@gmail.com'
   },
   {
     id: 4,
-    name: 'Patricia Lebsack',
-    username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
+    name: 'Chriss',
+   
+    email: 'criss@gmail.com'
   },
   {
     id: 5,
     name: 'Chelsey Dietrich',
-    username: 'Kamren',
     email: 'Lucio_Hettinger@annie.ca'
   },
   {
     id: 6,
     name: 'Mrs. Dennis Schulist',
-    username: 'Leopoldo_Corkery',
+    
     email: 'Karley_Dach@jasper.info'
   },
   {
     id: 7,
     name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
+    
     email: 'Telly.Hoeger@billy.biz'
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
-    username: 'Maxime_Nienow',
+    name: 'Nicholas Runol',
+    
     email: 'Sherwood@rosamond.me'
   },
   {
     id: 9,
     name: 'Glenna Reichert',
-    username: 'Delphine',
+   
     email: 'Chaim_McDermott@dana.io'
   },
   {
     id: 10,
     name: 'Clementina DuBuque',
-    username: 'Moriah.Stanton',
+   
     email: 'Rey.Padberg@karina.biz'
-  }
+  },
+  {
+    id: 11,
+    name: 'Ptbotgourou',
+    email: 'Ptbotgourou@gmail.com'
+  },
+  {
+   id:12,
+    name: 'BotMultichill',
+    email: 'BotMultichill@gmail.com'
+  },
+  {
+    id: 13,
+    name: 'Chobot',
+    email: 'Chobot@gmail.com'
+  },
+  {
+    id: 14,
+    name: 'Fyrilacroc',
+    email: 'Fyrilacroc@gmail.com'
+  },
+  {
+    id: 15,
+    name: 'TjBot',
+    email: 'TjBot@gmail.com'
+  },
+  {
+    id: 16,
+    name: 'Jackie Rey',
+    email: 'Jackie@gmail.com'
+  },
+  {
+    id: 17,
+    name: 'Kamilkaze',
+    email: 'Kamilkaze@gmail.com'
+  },
+  {
+    id: 18,
+    name: 'Rubin Stig',
+    email: 'Rubin@gmail.com'
+  },
+  {
+    id: 19,
+    name: 'Fox Silvoen',
+    email: 'Silvoen@gmail.com'
+  },
+   {
+    id: 20,
+    name: 'Pipep Synthe',
+    email: 'Pipep@gmail.com'
+  },
+  {
+    id: 21,
+    name: "D'oh Dinawik",
+    email: 'Dinawikz@gmail.com'
+  },
+   {
+    id: 22,
+    name: 'MSCL Roaw',
+    email: 'Roaw@gmail.com'
+  },
+  {
+    id: 23,
+    name: 'Vago-bot',
+    email: 'Vago-bot@gmail.com'
+  },
+  {
+    id: 24,
+    name: 'DSisyh Gutz',
+    email: 'DSisyh@gmail.com'
+  },
+  {
+    id: 25,
+    name: 'Carsrac',
+    email: 'Carsrac@gmail.com'
+  },
+  {
+    id: 26,
+    name: 'Sasso',
+    email: 'Sasso@gmail.com'
+  },
+   {
+    id: 27,
+    name: 'Rad vrs',
+    email: 'Radvrs@gmail.com'
+  },
+  {
+    id: 28,
+    name: 'Obersachse',
+    email: 'Obersachse@gmail.com'
+  },
+  {
+    id: 29,
+    name: 'Rascol',
+    email: 'Rascol@gmail.com'
+  },
+   {
+    id: 30,
+    name: 'Butko Botarate',
+    email: 'Butko@gmail.com'
+  },
+  {
+    id: 31,
+    name: 'Ribot',
+    email: 'Ribot@gmail.com'
+  },
+   {
+    id: 32,
+    name: 'Jotterobot',
+    email: 'Jotterobot@gmail.com'
+  },
+  {
+    id: 33,
+    name: 'Wiki Dreamer',
+    email: 'WikiDreamer@gmail.com'
+  },
+  {
+    id: 34,
+    name: 'Darkice Dorgan',
+    email: 'Darkice@gmail.com'
+  },
+  {
+    id: 35,
+    name: 'Robbot Abiot',
+    email: 'Abiot@gmail.com'
+  },
+  {
+    id: 36,
+    name: 'Alecs Goliath',
+    email: 'Goliath@gmail.com'
+  },
+   {
+    id: 37,
+    name: 'Pixel JY',
+    email: 'Pixel@gmail.com'
+  },
+  {
+    id: 38,
+    name: 'Bodhisattva',
+    email: 'Bodhisattva@gmail.com'
+  },
+  {
+    id: 39,
+    name: 'Brick XXN',
+    email: 'Brick@gmail.com'
+  },
+   {
+    id: 40,
+    name: 'Alma Nallim',
+    email: 'Nallim@gmail.com'
+  },
+  {
+    id: 41,
+    name: 'Avic Cocu',
+    email: 'Avic@gmail.com'
+  },
+  {
+    id: 42,
+    name: 'Palica Rotlink',
+    email: 'Palica@gmail.com'
+  },
+  {
+    id: 43,
+    name: 'Hercule Maurits',
+    email: 'Hercule@gmail.com'
+  },
+  {
+    id: 44,
+    name: 'HYW Tuvic',
+    email: 'HYWTuvic@gmail.com'
+  },
+  {
+    id: 45,
+    name: 'Sottile',
+    email: 'Sottile@yahoo.com'
+  },
+   {
+    id: 46,
+    name: 'Hashar',
+    email: 'Hashar@yahoo.com'
+  },
+   {
+    id: 47,
+    name: 'DixonD',
+    email: 'DixonD@yahoo.com'
+  },
+  {
+    id: 48,
+    name: 'Siw Mins',
+    email: 'Siw@yahoo.com'
+  },
+   {
+    id: 49,
+    name: 'Darafsh',
+    email: 'Darafsh@yahoo.com'
+  },
+  {
+    id: 50,
+    name: 'Timbot',
+    email: 'Timbot@yahoo.com'
+  },
+  {
+    id: 51,
+    name: 'Bourget',
+    email: 'Bourget@yahoo.com'
+  },
+  {
+    id: 52,
+    name: 'Maaty',
+    email: 'Matty@yahoo.com'
+  },
+  {
+    id: 53,
+    name: 'Totty',
+    email: 'Totty@yahoo.com'
+  },
+  {
+    id: 54,
+    name: 'Dotty',
+    email: 'Dotty@yahoo.com'
+  },
+   {
+    id: 55,
+    name: 'RobotX',
+    email: 'RobotX@yahoo.com'
+  },
+  {
+    id: 56,
+    name: 'Wikitanvir',
+    email: 'Wikitanvir@yahoo.com'
+  },
+  {
+    id: 57,
+    name: 'Numbo3',
+    email: 'Numbo3@yahoo.com'
+  },
+  {
+    id: 58,
+    name: 'Thijs!bot',
+    email: 'Thijs!bot@yahoo.com'
+  },
+  {
+    id: 59,
+    name: 'Volkov',
+    email: 'Volkov@yahoo.com'
+  },
+  {
+    id: 60,
+    name: 'TxIkI Xq',
+    email: 'TxIkI@yahoo.com'
+  },
+   {
+    id: 61,
+    name: 'TxIkI Xq',
+    email: 'TxIkI@yahoo.com'
+  },
+  {
+    id: 62,
+    name: 'Merllw GUT',
+    email: 'Merllw@yahoo.com'
+  },
+  {
+    id: 63,
+    name: 'BRD First',
+    email: 'BRDFirst@yahoo.com'
+  },
+  {
+    id: 64,
+    name: 'Wafy XEO',
+    email: 'Wafy@yahoo.com'
+  },
+  {
+    id: 65,
+    name: 'AddBot',
+    email: 'AddBot@yahoo.com'
+  },
+  {
+    id: 66,
+    name: 'Alleborgo',
+    email: 'Alleborgo@yahoo.com'
+  },
+{
+    id: 67,
+    name: 'Wikitawir',
+    email: 'Wikitawir@yahoo.com'
+  },
+  {
+    id: 68,
+    name: 'Robot Qwistrix',
+    email: 'Qwistrix@yahoo.com'
+  },
+  {
+    id: 69,
+    name: 'JanD',
+    email: 'JanD@yahoo.com'
+  },
+  {
+    id: 70,
+    name: 'L.A AWB',
+    email: 'L.AAWB@yahoo.com'
+  },
 ];
+
+
